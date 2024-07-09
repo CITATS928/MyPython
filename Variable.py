ebb0ed9f692dcc5414ae9fccebce4d4e72c7d53c
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-09 10:41:49 -0400

    10:41 AM

---
+++ Variable.py
@@ -0,0 +1,7 @@
+# Six basic data type
+# Bool. Number, String, Tuple, List, Set, Dictionary
+# Bool, Number, String, Tuple 属于不可变类型
+"""
+当分配了x=1之后，如果再分配x=2，会分配给x一个新的地址，
+其他几个会直接给原地址抹掉重新赋值
+"""
---
+++ hello.py
@@ -0,0 +1,8 @@
+#Ask user to input the name
+name = input("whats your name?")
+"""
+output
+welcome info
+"""
+print(f"Hello, {name}")
+print("Have a nice day.")
